2cd431c3dd098e10146d5b6aa21afa314f0097b6
maxaraujo <maximillian.alex@gmail.com>
2017-04-25 08:20:00 -0300

    Parte 3 - Commit inicial. (parte 2 sem identação)

---
+++ topzera.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <dirent.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+void limpaString(char *str){
+	strcpy(str,"");
+}
+
+void arquivoNomeProcesso(char *str, char processo[]){
+		strcpy(str, "/proc/");				
+		strcat(str, processo);		
+		strcat(str, "/task/");			
+		strcat(str, processo);		
+		strcat(str, "/comm");
+}
+
+void arquivoFilhos(char *str, char processo[]){
+		strcpy(str, "/proc/");				
+		strcat(str, processo);		
+		strcat(str, "/task/");			
+		strcat(str, processo);		
+		strcat(str, "/children");
+}
+
+void retornarNomeProcesso(char str[], char *str2){
+	FILE *fp;
+	char word[100];
+	
+	fp = fopen (str,"r");	
+		
+		while (fscanf(fp,"%s", word) != EOF) { 
+			strcat(str2, word);
+			printf("%s \n", str2);
+			limpaString(str2);
+		}		
+	fclose(fp);
+}
+
+int func(char str[], int espaco) 
+{
+	int a;	
+	FILE *fp;
+	char str2[100], pchTemp[100], str3[100], word[100];
+	limpaString(str2);
+	limpaString(pchTemp);
+	fp = fopen (str,"r");	      
+
+	while (fscanf(fp,"%s", word) != EOF) {    
+		//for (a=0; a<espaco;a++)
+				 	//strcat(pchTemp, "__");
+		
+		arquivoNomeProcesso(str3, word);	
+		retornarNomeProcesso(str3,pchTemp);
+		
+		arquivoFilhos(str2, word);
+		
+		func(str2, espaco+1);
+	}
+	fclose(fp);
+	return 0;	
+}
+
+int main(){
+	char str[100], str2[100];
+	
+	arquivoNomeProcesso(str,"1");
+	retornarNomeProcesso(str,str2);
+	func("/proc/1/task/1/children", 1);
+    return 0;
+}
